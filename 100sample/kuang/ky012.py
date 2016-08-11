8215e8ee913c223c0fa11234d4885f58c7f5cd9b
kuangyu1997 <kuangyu1997@hotmail.com>
2016-08-11 19:59:31 +0800

    ky012

--- 100sample/kuang/ky012.py
+++ 100sample/kuang/ky012.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import math
-
 h = 0
 leap = 1
 from math import sqrt
-from sys import stdout
 for m in range(101,201):
-    k = int(sqrt(m + 1))
+    k = int(sqrt(m))
     for i in range(2,k + 1):
         if m % i == 0:
-            leap = 0    
-                break
-    if 
-
+            leap = 0
+            break
+    if leap == 1:
+        print '%d' % m
+        h += 1
+        if h % 10 == 0:
+            print ''
+    leap = 1
+print 'The total is %d' % h
